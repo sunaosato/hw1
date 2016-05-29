9bc7a197f6c7e574270f193394afe2b8c734b022
STEPER <step@step>
2016-05-30 00:00:50 +0900

    len -> len - 1

--- sort.c
+++ sort.c
@@ -6,25 +6,28 @@
 char sort(char* str2){
   int i ,j,k,len = strlen(str2);
   char a;
-  
-  for(k=0;k < len;k++){
-    str2[k]=tolower(str2[k]);
-  }
-  
-  if(len > 1){
-  for(i = 0; i < len;i++){
-    for(j = i+1;j < len;j++){
 
-      if(str2[i]>str2[j]){
-	a = str2[i];
-	str2[i]= str2[j];
-	str2[j]= a;
+  printf("%d\n",len);
+  
+    for(k=0;k < len;k++){
+    str2[k]=tolower(str2[k]);
+    }
+  
+    if(len > 1){
+      for(i = 0; i < len-1;i++){
+	for(j = i+1;j < len;j++){
+
+	  if(str2[i] >= str2[j]){
+	    a = str2[i];
+	    str2[i]= str2[j];
+	    str2[j]= a;
+	  }
+	}
       }
     }
-  }
 
-      printf("sort:%s\n",str2);
-  }
-
-
+    printf("sort:%s\n",str2);
 }
+
+
+
