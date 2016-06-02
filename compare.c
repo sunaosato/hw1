23f6d7ee04a805265c20784545d686cccba6b249
STEPER <step@step>
2016-06-03 00:00:44 +0900

    commentout

--- compare.c
+++ compare.c
@@ -8,6 +8,7 @@ void compare(char* str3){
     char dic[NUM];
     int lens =strlen(str3);
     FILE* fp = fopen("/usr/share/dict/words","r");
+    //FILE* fp = fopen("dictionary.txt","r");
     
     if(fp == NULL){
      printf("Fail to open file.");
@@ -19,21 +20,41 @@ void compare(char* str3){
     while(fgets(dic,NUM,fp)!= NULL){
       char dic2[NUM];
       int l;
-      for(l=0;l < strlen(dic);l++){
-	if(dic[l]==0x0a){
-	  dic[l]=0x00;
-	  break;
+      //  int l2;
+      strcpy(dic2,dic); //DICをDIC2へコピー
+      //int lend1 =strlen(dic);
+
+      
+      /*for(l2=0;l2<strlen(dic2);l2++){
+	if(isalpha(dic2[l2])==0){
+	  int a;
+	  a = l2;
+	  while(dic2[a]!=0x0a){
+	  dic2[a]=dic2[a+1];
+	  a++;
+	  }
+	}
+	}*/
+      //printf("change to 0x0a:%s\n",dic2);
+      
+      for(l=0;l < strlen(dic2);l++){
+	if(dic2[l]==0x0a){
+	  dic2[l]=0x00;
+	    break;
 	}
       }
-      
-      strcpy(dic2,dic); //DICをDIC2へコピー
+
+
+ 
+      //printf("change to 0x00:%s\n",dic2);     
+
       sort(dic2);
-      int lend = strlen(dic2);
+      int lend2 = strlen(dic2);
       //  printf("DIC:%s,DIC2:%s,LEND:%d,LENS:%d\n",dic,dic2,lend,lens);
       int i,j=0,k=0,n=0;
       // printf("FIRST K:%d\n",k);
       
-      for(i=0;i < lend; i++){
+      for(i=0;i < lend2; i++){
 	for(j = n; j< lens;j++){
 	  if(str3[j]== dic2[i]){//文字が一致すればKを増やす
 	    k++;
@@ -48,10 +69,10 @@ void compare(char* str3){
       // printf("COMPARE MAX K:%d %d\n",max,k);
       if(k > max){//Kが一番大きかったとき
 	//printf("K :%d,LEND:%d\n",k,lend);
-	if(k == lend){//KがDIC2の長さと一致したら
+	if(k == lend2){//KがDIC2の長さと一致したら
 	  max = k;//MAXをKに更新
 	  int m;
-	  for(m=0; m< lend;m++){
+	  for(m=0; m< lend2;m++){
 	  mstr[m] = dic[m];//一番長い文字列を格納
 	  }
 	  //printf("更新しました。mstr:%s\n",mstr);
